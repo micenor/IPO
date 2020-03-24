25dde08261b27a6a1e8bedbcb895f97c32211b0c
rodri666a <rodri6diezalonso@gmail.com>
2020-03-24 17:36:32 +0100

    Cronometro basico

--- app/src/main/java/com/example/forest/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/forest/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.example.forest.ui.home;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -15,14 +18,58 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.forest.R;
 
 public class HomeFragment extends Fragment {
-
-
+TextView tiempo;
+Vibrator satysfier;
+Button suma,resta,empezar;
+int tempo=5000;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+         TextView textView = root.findViewById(R.id.text_home);
+         tiempo = root.findViewById(R.id.text_tiempo);
+         suma= root.findViewById(R.id.button);
+         resta = root.findViewById(R.id.button2);
+         empezar = root.findViewById(R.id.button3);
+         tiempo.setText(""+(tempo/1000)/60);
+         empezar.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 empezar.setVisibility(View.INVISIBLE);
+                 new CountDownTimer(tempo,1000){
+
+                     @Override
+                     public void onTick(long millisUntilFinished) {
+                         tiempo.setText("Quedan "+(millisUntilFinished/1000)/60+":"+(millisUntilFinished/1000)%60);
+
+                     }
+
+                     @Override
+                     public void onFinish() {
+                         tiempo.setText("tiempoooo");
+                         empezar.setVisibility(View.VISIBLE);
+                     }
+                 }.start();
+             }
+         });
+         suma.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 tempo+=300000;
+                 tiempo.setText(""+(tempo/1000)/60);
+             }
+         });
+         resta.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 tempo-=300000;
+                 tiempo.setText(""+(tempo/1000)/60);
+             }
+         });
+
+
 
         return root;
     }
+
 }

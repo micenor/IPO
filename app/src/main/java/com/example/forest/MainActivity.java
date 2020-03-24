f2363cd1fb9777c90610ee89cb99b79a580f5864
chexu99 <48331531+chexu99@users.noreply.github.com>
2020-03-24 14:48:23 +0100

    mwnu updated

--- app/src/main/java/com/example/forest/MainActivity.java
+++ app/src/main/java/com/example/forest/MainActivity.java
@@ -39,13 +39,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_home, R.id.nav_amigos, R.id.nav_apps,
+                R.id.nav_retos, R.id.nav_tienda, R.id.nav_baul)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);

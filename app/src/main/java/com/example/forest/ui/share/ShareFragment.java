e255e36d6cda7f8dee7989edc605b0cda286a846
Miguel <micenor88@gmail.com>
2020-03-25 19:07:39 +0100

    Cosas varias

--- app/src/main/java/com/example/forest/ui/share/ShareFragment.java
+++ app/src/main/java/com/example/forest/ui/share/ShareFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,15 +15,27 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.forest.R;
 
+import org.w3c.dom.Text;
+
 public class ShareFragment extends Fragment {
 
     private ShareViewModel shareViewModel;
 
+    TextView monedas;
+    ImageButton comprar;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         shareViewModel =
                 ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_tienda, container, false);
+        ImageButton comprar = root.findViewById(R.id.imageButton);
+        final TextView monedas = root.findViewById(R.id.textView);
+        comprar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                monedas.setText("500");
+            }
+        });
         return root;
     }
 }
